a6ebb4d061df02084d23e6427524d19d1d1a1434
LPPL <lephuocphulam@gmail.com>
2025-05-30 14:59:41 +0700

    update

---
+++ Slot6/exercise7/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import React from 'react';
+import CardColumnsDemo from './CardColumnsDemo';
+
+function App() {
+  return (
+    <div>
+      <CardColumnsDemo />
+    </div>
+  );
+}
+
+export default App;

5bab171b705df829cc91cb49b705a3c81a1a4481
LPPL <lephuocphulam@gmail.com>
2025-06-06 13:57:32 +0700

    update

---
+++ Slot8/exercise1/src/components/MyProfile.jsx
@@ -0,0 +1,13 @@
+// MyProfile.jsx
+import React from 'react';
+
+function MyProfile() {
+  return (
+    <div>
+      <h1>Lê Phước Phú Lâm</h1>
+      <p>Xin chào mọi người </p>
+    </div>
+  );
+}
+
+export default MyProfile;

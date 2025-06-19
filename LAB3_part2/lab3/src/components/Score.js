064dbaa305354399c8bc0ebba28b9425d0ebea9c
LPPL <lephuocphulam@gmail.com>
2025-06-19 18:03:08 +0700

    update

---
+++ LAB3_part2/lab3/src/components/Score.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Score({ score, total, onRetry }) {
+  return (
+    <div>
+      <h2>Quiz Ended</h2>
+      <p>Your Score: {score} / {total}</p>
+      <button onClick={onRetry}>Try Again</button>
+    </div>
+  );
+}
+
+export default Score;

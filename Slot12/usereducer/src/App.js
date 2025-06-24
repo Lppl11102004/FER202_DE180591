aee0edd0d8609f2b3e34f30d4f2c0cc5ccfa2c8f
LPPL <lephuocphulam@gmail.com>
2025-06-24 18:20:58 +0700

    update

--- Slot12/usereducer/src/App.js
+++ Slot12/usereducer/src/App.js
@@ -4,18 +4,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // Đổi component ở đây để test từng bài
 // import Counter from "./components/Counter";
 // import ChangeNameAge from "./components/ChangeNameAge";
-// import ItemList from "./components/ItemList";
+import ItemList from "./components/ItemList";
 // import QuestionBank from "./components/QuestionBank";
-import QuestionBankPlus from "./components/QuestionBankPlus";
+// import QuestionBankPlus from "./components/QuestionBankPlus";
 
 function App() {
   return (
     <div className="App">
       {/* <Counter /> */}
       {/* <ChangeNameAge /> */}
-      {/* <ItemList /> */}
+      <ItemList />
       {/* <QuestionBank /> */}
-      <QuestionBankPlus />
+      {/* <QuestionBankPlus /> */}
     </div>
   );
 }

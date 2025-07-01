edfbf8ecc8115a885bc20cbded4acb246ef47b71
LPPL <lephuocphulam@gmail.com>
2025-07-01 20:27:55 +0700

    update

---
+++ Slot14/router/src/components/PostDetail.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+function PostDetail() {
+  const { id } = useParams();
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/posts/${id}`)
+      .then(res => res.json())
+      .then(data => setPost(data))
+      .catch(err => console.error('Error loading post:', err));
+  }, [id]);
+
+  if (!post) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <div>
+      <h2>{post.title}</h2>
+      <p>{post.content}</p>
+    </div>
+  );
+}
+
+export default PostDetail;
